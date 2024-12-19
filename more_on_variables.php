e6bbb461ab2b7d2c357836ccbc06ac672a569eaa
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-12-19 21:13:09 +0530

    more on variables

---
+++ more_on_variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "More on Variables" ?></title>
+</head>
+<body>
+    <?php echo "<h1>More on PhP variables</h1>" ?>
+    <h4>Rules for creating variables in php</h4>
+    <ul>
+        <li>Starts with a $ sign</li>
+        <li>Cannot start with special charecter</li>
+        <li>start with $ sign, followed by name of the variable </li>
+        <li>variable name must start with letter or the underscore</li>
+        <li>a variable name can only contain the alpha numeric values</li>
+    </ul>
+</body>
+</html>
