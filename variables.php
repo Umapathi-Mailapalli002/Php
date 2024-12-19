a5074f3d40f22d1008abb2f08fc5939e01583acb
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-12-19 12:19:43 +0530

    variables in php

---
+++ variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title><?php echo "this is "; echo "title"?></title>
+</head>
+
+<body>
+    <?php
+    // Varibles are the containers for storing information 
+    // in php the variables start with a $(doller) symbol
+    // to breake the line use <br/> tag in echo
+    //variables are case sensitive in php
+    //php is a loosily type language means not have to declare data type
+    // later it add the strict declaration of data type
+    $name = "Ravi";
+    $passion = "Web Developer";
+    echo "Hi, iam $name and iam a passionate $passion";
+    ?>
+   
+</body>
+
+</html>
