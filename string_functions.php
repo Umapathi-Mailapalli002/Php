e7cb22b6ed0d1b5947cd7f09766284b3a55cacbb
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-12-20 09:18:09 +0530

    functions

--- string_functions.php
+++ string_functions.php
@@ -10,14 +10,19 @@
     $name = "Elon Musk";
     echo $name;
     echo "<br/>";
-
     //to concatinate the strings use . and strlen is used find the length of the string
     echo "length of my string is " . strlen($name) . "<br/>";
 
     //to count the words use str_word_count
-    echo "number of words in $name is ". str_word_count($name)
+    echo "number of words in $name is ". str_word_count($name);
     echo "<br/>";
-    
+    ?>
+
+    <?php
+    function username($username){
+        return "this is your username $username";
+    }
+    echo username("ravi");
     ?>
 </body>
 </html>
