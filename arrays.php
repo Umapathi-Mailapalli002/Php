280785535949fecbc2047c49c8956d0919ee5ca4
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-12-21 08:32:01 +0530

    associative array

--- arrays.php
+++ arrays.php
@@ -8,6 +8,8 @@ Indexed arrays - Arrays with a numeric index
 Associative arrays - Arrays with named keys
 Multidimensional arrays - Arrays containing one or more arrays
 
+Array items can be of any data type
+$myArr = array("Volvo", 15, ["apples", "bananas"], myFunction);
 */
 
 //Indexed Array
@@ -36,4 +38,38 @@ foreach($array as $item){
     [ 2 ] => Bca
 ) */
 
+
+echo "<br>";
+echo "<br>";
+//Assiociative Arrays
+//in this we can add the keys for each values in arrays
+
+$data = array("name" => "nitin", "age" => 20, "education" => "Bca");
+
+//this we can also print with var_dump with its types
+echo '<pre>';
+var_dump($data);
+echo '</pre>';
+//output
+/* array(3) {
+    ["name"]=>
+    string(5) "nitin"
+    ["age"]=>
+    int(20)
+    ["education"]=>
+    string(3) "Bca"
+  } */
+
+
+  echo "<br>";
+  foreach($data as $key => $item):
+  echo "$key is $item";
+  echo "<br>";
+endforeach
+
+//output
+
+/* name is nitin
+age is 20
+education is Bca */
 ?>
